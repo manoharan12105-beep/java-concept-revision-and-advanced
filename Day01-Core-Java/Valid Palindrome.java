ecd3da2fa81801a6440d06d76dbb571afe14215f
manoharan12105-beep <manoharan12105@gmail.com>
2026-02-14 20:36:19 +0530

    Add Valid Palindrome solution class

---
+++ Day01-Core-Java/Valid Palindrome.java
@@ -0,0 +1,22 @@
+/*
+Leetcode 125. Valid Palindrome
+Time Complexity  : O(n)
+Space Complexity : O(n)
+*/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if ( s.charAt(left++) != s.charAt(right--) ) {
+                return false;
+            }
+        }
+        
+        return true;        
+    }
+}
+
