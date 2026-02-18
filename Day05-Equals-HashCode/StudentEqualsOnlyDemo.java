709f612f9c232019a485717a65f093d6d1375d8f
manoharan12105-beep <manoharan12105@gmail.com>
2026-02-18 19:37:22 +0530

    Add equals method to Student class
    Implemented equals method for Student class to compare objects based on id and name.

--- Day05-Equals-HashCode/StudentEqualsOnlyDemo.java
+++ Day05-Equals-HashCode/StudentEqualsOnlyDemo.java
@@ -1,1 +1,50 @@
 
+import java.util.*;
+
+class Student {
+	int id;
+	String name;
+	Student(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+
+		if(obj == null || getClass() != obj.getClass())
+			return false;
+
+		Student s = (Student) obj;
+		return id == s.id && Objects.equals(name, s.name);
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		HashSet<Student> set = new HashSet<>();
+		Student s1 = new Student(1, "Mano");
+		Student s2 = new Student(1, "Mano");
+		Student s3 = new Student(2, "Doni");
+
+		set.add(s1);
+		set.add(s2);
+		set.add(s3);
+
+		System.out.println("Total Students in HashSet: " + set.size());
+		for(Student s : set)
+			System.out.println(s.id + " " + s.name);
+	}
+}
+/*
+Output : 
+
+Total Students in HashSet: 3
+1 Mano
+1 Mano
+2 Doni
+
+Link : https://onlinegdb.com/GfMv_W3p7
+*/
