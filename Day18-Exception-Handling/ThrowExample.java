5807eeaff14f89e5ce20cbd26e4d46dd39a13a01
manoharan12105-beep <manoharan12105@gmail.com>
2026-03-03 21:17:02 +0530

    Create ThrowExample.java

---
+++ Day18-Exception-Handling/ThrowExample.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void validateAge(int age){
+        
+        if (age < 18) {
+            throw new IllegalArgumentException("Age must be 18 or above.");
+        }
+
+        System.out.println("Age is valid.");
+    }
+
+    public static void main(String[] args){
+
+        try{
+            validateAge(16);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
+
+//Exception caught: Age must be 18 or above.
