ced2038e40b97e39bf7a6d23d734d37d145283fb
manoharan12105-beep <manoharan12105@gmail.com>
2026-03-09 21:41:56 +0530

    Create Student.java

---
+++ Day24-Hibernate-Basics/demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+  
+  @Id
+  private int rollNo;
+  private String name;
+  private int age;
+  
+  public int getRollNo() {
+    return rollNo;
+  }
+  public String getName() {
+    return name;
+  }
+  public int getAge() {
+    return age;
+  }
+  
+  public void setRollNo(int rollNo) {
+    this.rollNo = rollNo;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return "Student [rollNo=" + rollNo + ", name=" + name + ", age=" + age + "]";
+  }
+
+
+  
+}
