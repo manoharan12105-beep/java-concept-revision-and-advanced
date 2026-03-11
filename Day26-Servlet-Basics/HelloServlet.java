861ba406efaf0c31a14e56941fef02743ca0b9a3
manoharan12105-beep <manoharan12105@gmail.com>
2026-03-11 21:06:56 +0530

    Create HelloServlet.java

---
+++ Day26-Servlet-Basics/HelloServlet.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Welcome Mano 🚀</h1>");
+        out.println("<p>Day 26 - Servlet Basics</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
